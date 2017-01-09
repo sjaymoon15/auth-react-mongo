ad7de3bdc50ca33ce569155228549459668cc6c2
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-09 12:48:50 -0500

    encrypt user password using bcrypt-nodejs

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,7 +4,7 @@ exports.signup = function(req, res, next){
 	const email = req.body.email;
 	const password = req.body.password;
 	
-	if(!email|| !password){
+	if(!email || !password){
 		return res.status(422).send({ error: "You must provide email and password" });
 	}
 	// see if a user with the given email exists
