22eb68c69b478a63f2cabcabfda1db834ce9c67f
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-18 15:04:34 -0500

    solve CORS issue on the server side

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //DB setup
 mongoose.connect('mongodb://localhost/auth'); 
@@ -14,6 +15,7 @@ mongoose.connection.once('connected', function() {
 });
 //App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 module.exports = function(app){
 	app.get('/', requireAuth, function(req, res){
-		res.send({hi: 'there'});
+		res.send({ message: 'Super secret code is ABC123'});
 	});
 	app.post('/signin', requireSignin, Auth.signin);
 	app.post('/signup', Auth.signup);
